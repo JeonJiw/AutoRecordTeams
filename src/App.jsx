f62edb91c7a4bbb84d6e58addfb4ab8ac753880f
jiwonjeon <j.jeon339@mybvc.ca>
2024-03-07 14:26:24 -0700

    Setting msgraph

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './styles/App.css';
+import { PageLayout } from './components/PageLayout';
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
+import Button from 'react-bootstrap/Button';
+import { loginRequest } from './authConfig';
+import { callMsGraph } from './graph';
+import { ProfileData } from './components/ProfileData';
+import { createOrUpdateMeetingWithAutoRecording } from './graph';
+
+
+
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance
+            .acquireTokenSilent({
+                ...loginRequest,
+                account: accounts[0],
+            })
+            .then((response) => {
+                callMsGraph(response.accessToken).then((response) => setGraphData(response));
+            });
+    }
+
+    function EnableAutoRecording() {
+        instance.acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0],
+        }).then((response) => {
+            createOrUpdateMeetingWithAutoRecording(response.accessToken).then((meetingData) => {
+                console.log('Meeting Created with Auto-Recording:', meetingData);
+                // Handle meeting data here (e.g., display a message or update the UI)
+            });
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+                <Button variant="secondary" onClick={RequestProfileData}>
+                    Request Profile Information
+                </Button>
+                <Button variant="primary" onClick={EnableAutoRecording} style={{ marginLeft: '10px' }}>
+                    Enable Auto-Recording
+                </Button>
+            {graphData && (
+                <ProfileData graphData={graphData} />
+            )}
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to see your profile information.</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
